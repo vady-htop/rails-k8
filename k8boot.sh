4dfb455831a0671f0804cd70b9bfd33c9bf71d57
vardhaman shetti <vardhaman@deepsource.io>
2020-06-12 23:59:07 +0530

    Add url
    Signed-off-by: vardhaman shetti <vardhaman@deepsource.io>

--- k8boot.sh
+++ k8boot.sh
@@ -4,6 +4,12 @@ minikube start
 minikube addons enable ingress
 helm repo add stable https://kubernetes-charts.storage.googleapis.com && helm repo update
 cd helm/Jenkins
+kubectl create secret generic dockercredentials --from-file credentials.xml
+kubectl create serviceaccount helm
+kubectl create clusterrolebinding helm \
+  --clusterrole cluster-admin \
+  --serviceaccount=default:helm
 helm upgrade --install jenkins  stable/jenkins -f values.yaml
 MINIKUBE_IP=$(minikube ip)
-#sudo -- sh -c "echo $MINIKUBE_IP  "jenkins.dev.com" >> /etc/hosts"
+sudo -- sh -c "echo $MINIKUBE_IP  "www.jenkins.dev.com" >> /etc/hosts"
+sudo -- sh -c "echo $MINIKUBE_IP  "www.rails-app.com" >> /etc/hosts"
