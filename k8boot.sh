b4801c6634121a97ad755235d4bbee5a6e540a0f
vardhaman shetti <vardhaman@deepsource.io>
2020-06-12 22:14:38 +0530

    Add credentials and xml
    Signed-off-by: vardhaman shetti <vardhaman@deepsource.io>

--- k8boot.sh
+++ k8boot.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
 minikube start
-helm repo add stable https //kubernetes-charts.storage.googleapis.com/
+minikube addons enable ingress
+helm repo add stable https://kubernetes-charts.storage.googleapis.com && helm repo update
 cd helm/Jenkins
 helm upgrade --install jenkins  stable/jenkins -f values.yaml
+MINIKUBE_IP=$(minikube ip)
+#sudo -- sh -c "echo $MINIKUBE_IP  "jenkins.dev.com" >> /etc/hosts"
